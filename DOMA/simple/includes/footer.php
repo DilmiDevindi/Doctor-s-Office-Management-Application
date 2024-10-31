2371f3b573a5cc068fac786566f5ce0a306043c5
Dilmi Devindi <143577421+DilmiDevindi@users.noreply.github.com>
2024-10-31 23:27:40 +0530

    Add files via upload

---
+++ DOMA/simple/includes/footer.php
@@ -0,0 +1,18 @@
+<!--footer-->
+    
+    <style>
+.footer {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+
+  color: white;
+  text-align: center;
+}
+</style>
+
+<div class="footer">
+       <p>&copy; Doctor's Office Management Application</p>
+    </div>
+        <!--//footer-->
